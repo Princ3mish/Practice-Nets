35a1b44d99188abacf55c10ad24e2b30f74b66c4
Prince Mishra <pric3mxd@gmail.com>
2025-08-22 21:00:33 +0530

    Time: 31 ms (56.53%), Space: 51.7 MB (40.9%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -4,7 +4,7 @@ class Solution {
         List<List<Integer>> result = new ArrayList<>();
         
         for (int i = 0; i < nums.length - 2; i++) {
-            // Skip duplicates for i
+            
             if (i > 0 && nums[i] == nums[i - 1]) continue;
             
             int left = i + 1;
