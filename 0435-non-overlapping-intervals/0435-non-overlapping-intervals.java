cc892d2377cb0075ceea071e97a6270211870d2c
Prince Mishra <pric3mxd@gmail.com>
2025-09-05 14:56:23 +0530

    Time: 45 ms (92.32%), Space: 73.5 MB (71.56%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -5,14 +5,14 @@ class Solution {
         Arrays.sort(intervals, (a, b) -> a[1] - b[1]);
 
         int count = 0;
-        int prevEnd = intervals[0][1]; // Use the end time of the first interval (index 0, second element)
+        int prevEnd = intervals[0][1]; 
 
         for (int i = 1; i < intervals.length; i++) {
-            // Compare the start time of current interval with prevEnd
+            
             if (intervals[i][0] < prevEnd) {
-                count++; // Overlapping, remove this interval
+                count++; 
             } else {
-                prevEnd = intervals[i][1]; // Update prevEnd with current interval's end
+                prevEnd = intervals[i][1]; 
             }
         }
         return count;
