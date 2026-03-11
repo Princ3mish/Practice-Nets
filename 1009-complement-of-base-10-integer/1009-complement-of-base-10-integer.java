30ebc91859ae687af1c1106e0352b5cc1786b1b8
Prince Mishra <pric3mxd@gmail.com>
2026-03-11 09:27:08 +0530

    Time: 0 ms (100%), Space: 42.1 MB (66.82%) - LeetHub

---
+++ 1009-complement-of-base-10-integer/1009-complement-of-base-10-integer.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int bitwiseComplement(int n) {
+
+        if(n == 0) return 1;
+
+        int mask = Integer.highestOneBit(n) * 2 - 1;
+
+        return mask ^ n;
+    }
+}
