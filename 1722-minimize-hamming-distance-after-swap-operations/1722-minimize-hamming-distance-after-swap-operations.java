47abf0d36a25519e0c85b3672adcab48def15e1e
Prince Mishra <pric3mxd@gmail.com>
2026-04-21 18:54:02 +0530

    Time: 45 ms (81.72%), Space: 112.1 MB (86.02%) - LeetHub

---
+++ 1722-minimize-hamming-distance-after-swap-operations/1722-minimize-hamming-distance-after-swap-operations.java
@@ -0,0 +1,58 @@
+import java.util.*;
+
+class Solution {
+    public int minimumHammingDistance(int[] source, int[] target, int[][] allowedSwaps) {
+        int n = source.length;
+
+        DSU dsu = new DSU(n);
+        for (int[] swap : allowedSwaps) {
+            dsu.union(swap[0], swap[1]);
+        }
+        Map<Integer, Map<Integer, Integer>> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            int root = dsu.find(i);
+            map.putIfAbsent(root, new HashMap<>());
+            Map<Integer, Integer> freq = map.get(root);
+
+            freq.put(source[i], freq.getOrDefault(source[i], 0) + 1);
+        }
+        int mismatch = 0;
+
+        for (int i = 0; i < n; i++) {
+            int root = dsu.find(i);
+            Map<Integer, Integer> freq = map.get(root);
+
+            if (freq.getOrDefault(target[i], 0) > 0) {
+                freq.put(target[i], freq.get(target[i]) - 1);
+            } else {
+                mismatch++;
+            }
+        }
+
+        return mismatch;
+    }
+}
+
+class DSU {
+    int[] parent;
+
+    public DSU(int n) {
+        parent = new int[n];
+        for (int i = 0; i < n; i++) parent[i] = i;
+    }
+
+    public int find(int x) {
+        if (parent[x] != x)
+            parent[x] = find(parent[x]);
+        return parent[x];
+    }
+
+    public void union(int a, int b) {
+        int pa = find(a);
+        int pb = find(b);
+        if (pa != pb) {
+            parent[pa] = pb;
+        }
+    }
+}
