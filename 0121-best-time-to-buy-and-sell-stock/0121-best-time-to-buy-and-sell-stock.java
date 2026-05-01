4ff63df59618f3b43e0d0ca3853a725a78e7e3c5
Prince Mishra <pric3mxd@gmail.com>
2026-05-01 14:43:14 +0530

    Time: 1 ms (99.91%), Space: 94.6 MB (15.24%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,7 +1,7 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int minPrice = Integer.MAX_VALUE;
-        int maxProfit = 0 ;
+        int maxProfit = 0;
         for(int price:prices){
             if(price < minPrice){
                 minPrice = price;
