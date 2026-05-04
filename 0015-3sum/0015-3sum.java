4c9afd0bc17d229c067f420d2533b31581a63695
Prince Mishra <pric3mxd@gmail.com>
2026-05-04 13:36:43 +0530

    Time: 34 ms (60.38%), Space: 59.3 MB (45.68%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -16,7 +16,7 @@ class Solution {
                 if (sum == 0) {
                     result.add(Arrays.asList(nums[i], nums[left], nums[right]));
                     
-                    // Skip duplicates for left and right
+                    
                     while (left < right && nums[left] == nums[left + 1]) left++;
                     while (left < right && nums[right] == nums[right - 1]) right--;
                     
