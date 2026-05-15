c9a6f12f1945bb790fc13b564d5dce4f2dac6afe
Prince Mishra <pric3mxd@gmail.com>
2026-05-15 20:19:02 +0530

    Time: 0 ms (100%), Space: 46.7 MB (29.33%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -11,25 +11,21 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        if ( head == null || head.next == null ) return null;
-        ListNode slow = head ;
-        ListNode fast = head ;
-
-        while( fast != null && fast.next != null){
-            slow =  slow.next;
+        if(head == null || head.next == null)return null;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
             fast = fast.next.next;
-
-            if( slow == fast){
-                ListNode ptr = head;
-                while( ptr != slow){
-                    ptr =  ptr.next;
-                    slow = slow.next;
-
-                }
-                return ptr;
+            if(slow == fast){
+                slow = head;
+            while(slow != fast){
+                slow = slow.next;
+                fast = fast.next;
+            
+            }return slow;
             }
         }
         return null;
-
     }
 }
