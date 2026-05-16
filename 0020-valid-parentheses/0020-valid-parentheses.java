36ec0a43891763f30d5c7c7cca10eb559df8bcff
Prince Mishra <pric3mxd@gmail.com>
2026-05-17 01:19:51 +0530

    Time: 4 ms (39.76%), Space: 43.1 MB (72%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,23 +1,18 @@
-import java.util.*;
-
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        Map<Character, Character> pairs = Map.of(
-            ')', '(', 
-            ']', '[', 
-            '}', '{'
-        );
-
-        for (char c : s.toCharArray()) {
-            if (pairs.containsValue(c)) {
-                stack.push(c);
-            } else if (pairs.containsKey(c)) {
-                if (stack.isEmpty() || stack.pop() != pairs.get(c)) {
-                    return false;
-                }
+        Stack<Character> st = new Stack<>();
+        for(int i = 0 ; i < s.length(); i++){
+            char ch = s.charAt(i);
+            if(ch == '(' || ch == '{' || ch == '['){
+                st.push(ch);
+            }else{
+                if(st.isEmpty())return false;
+                char top = st.pop();
+                if((ch == ')' && top != '(')||
+                (ch == '}' && top != '{')||
+                (ch == ']' && top != '['))return false;
             }
         }
-        return stack.isEmpty();
+        return st.isEmpty();
     }
 }
