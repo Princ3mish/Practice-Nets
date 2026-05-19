d62bdc10037a06459bc6b44335ae1e3f99a70e29
Prince Mishra <pric3mxd@gmail.com>
2026-05-20 03:53:50 +0530

    Time: 0 ms (100%), Space: 43.5 MB (14.51%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -1,31 +1,29 @@
-import java.util.ArrayList;
-import java.util.List;
-
-// Definition for a binary tree node.
-// public class TreeNode {
-//     int val;
-//     TreeNode left;
-//     TreeNode right;
-//     TreeNode() {}
-//     TreeNode(int val) { this.val = val; }
-//     TreeNode(int val, TreeNode left, TreeNode right) {
-//         this.val = val;
-//         this.left = left;
-//         this.right = right;
-//     }
-// }
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<>();
-        postorder(root, result);
+        if(root == null){
+            return result;
+        }
+        result.addAll(postorderTraversal(root.left));
+        result.addAll(postorderTraversal(root.right));
+        result.add(root.val);
         return result;
-    }
-    
-    private void postorder(TreeNode node, List<Integer> result) {
-        if (node == null) return;
-        postorder(node.left, result);
-        postorder(node.right, result);
-        result.add(node.val);
+
+        
     }
 }
