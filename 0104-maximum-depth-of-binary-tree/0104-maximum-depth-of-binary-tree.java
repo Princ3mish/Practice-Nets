5f6cff96fc0c1e466750b2d3fe83ca2d9bf1707a
Prince Mishra <pric3mxd@gmail.com>
2026-05-21 00:16:23 +0530

    Time: 0 ms (100%), Space: 44.3 MB (79.53%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -18,10 +18,8 @@ class Solution {
         if(root == null){
             return 0;
         }
-        int leftDepth = maxDepth(root.left);
-
-        int rightDepth = maxDepth(root.right);
-
-        return 1 + Math.max(leftDepth,rightDepth);
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return 1 + Math.max(left,right);
     }
 }
