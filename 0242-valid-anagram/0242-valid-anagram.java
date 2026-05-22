155e40c3984429024c3ff31e0be989142a016ee0
Prince Mishra <pric3mxd@gmail.com>
2026-05-22 21:47:36 +0530

    Time: 6 ms (45.06%), Space: 44.6 MB (60.48%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,12 +3,12 @@ class Solution {
         if(s.length() != t.length()){
             return false;
         }
-        int[] count =  new int[26];
-        for(int i = 0 ; i < s.length() ;i++){
+        int[] count = new int[26];
+        for(int i = 0 ; i < s.length() ; i++){
             count[s.charAt(i) - 'a']++;
             count[t.charAt(i) - 'a']--;
         }
-        for(int val : count ){
+        for(int val : count){
             if(val != 0){
                 return false;
             }
