08157843d66723a266e6521b559df618cbff7caa
Prince Mishra <pric3mxd@gmail.com>
2026-05-24 15:49:28 +0530

    Time: 10 ms (87.43%), Space: 46.6 MB (30.29%) - LeetHub

---
+++ 1340-jump-game-v/1340-jump-game-v.java
@@ -0,0 +1,43 @@
+import java.util.Arrays;
+
+class Solution {
+    private int[] dp;
+
+    public int maxJumps(int[] arr, int d) {
+        int n = arr.length;
+        dp = new int[n];
+        Arrays.fill(dp, -1);
+
+        int maxVisited = 0;
+        for (int i = 0; i < n; i++) {
+            maxVisited = Math.max(maxVisited, dfs(arr, d, i));
+        }
+
+        return maxVisited;
+    }
+
+    private int dfs(int[] arr, int d, int i) {
+        if (dp[i] != -1) {
+            return dp[i];
+        }
+
+        int n = arr.length;
+        int maxJumpsFromI = 1;
+
+        for (int x = 1; x <= d && i + x < n; x++) {
+            if (arr[i + x] >= arr[i]) {
+                break;
+            }
+            maxJumpsFromI = Math.max(maxJumpsFromI, 1 + dfs(arr, d, i + x));
+        }
+
+        for (int x = 1; x <= d && i - x >= 0; x++) {
+            if (arr[i - x] >= arr[i]) {
+                break;
+            }
+            maxJumpsFromI = Math.max(maxJumpsFromI, 1 + dfs(arr, d, i - x));
+        }
+
+        return dp[i] = maxJumpsFromI;
+    }
+}
