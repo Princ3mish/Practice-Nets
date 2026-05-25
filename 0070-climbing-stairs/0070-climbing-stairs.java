d5f3411e2be5b215fff2fbc8cb430bcadd501e0c
Prince Mishra <pric3mxd@gmail.com>
2026-05-25 13:11:40 +0530

    Time: 0 ms (100%), Space: 42 MB (71.16%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,16 +1,14 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n <= 2) return n;
-        int first = 1;
-        int second = 2;
-        for(int i = 3 ; i <=n ;i++){
-            int current = first + second;
-            first = second;
-            second = current;
-
-
+        if(n == 1 ){
+            return 1;
         }
-        return second;
-
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+        for(int i = 3 ; i <= n ;i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 }
