9cf3c7490f7152e07074460b0b3ff6f76b25657a
Prince Mishra <pric3mxd@gmail.com>
2026-05-25 11:37:32 +0530

    Time: 22 ms (90.75%), Space: 91 MB (9.77%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i = 0 ; i < nums.length ; i++){
+            int curr = nums[i];
+        if(map.containsKey(curr)){
+            int prev = map.get(curr);
+        if(i - prev <= k){
+            return true;
+        }
+        }
+        map.put(curr,i);
+        }
+        return false;
+        
+    }
+}
