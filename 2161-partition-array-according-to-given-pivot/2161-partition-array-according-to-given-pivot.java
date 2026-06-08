de96201fe242fa877e1eab41aab5a17aeceea6e4
Prince Mishra <pric3mxd@gmail.com>
2026-06-08 22:06:24 +0530

    Time: 4 ms (99.24%), Space: 160 MB (49.83%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
@@ -3,18 +3,21 @@ class Solution {
         int n = nums.length;
         int[] ans = new int[n];
         int left = 0;
-        int right = n-1;
-        for(int i = 0 , j = n-1 ; i<n ; i++,j--){
-            if(nums[i] < pivot){
+        int right = n - 1;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] < pivot) {
                 ans[left++] = nums[i];
             }
-            if(nums[j] > pivot){
+        }
+        for (int j = n - 1; j >= 0; j--) {
+            if (nums[j] > pivot) {
                 ans[right--] = nums[j];
             }
         }
-        while(left <= right){
+        while (left <= right) {
             ans[left++] = pivot;
         }
+        
         return ans;
     }
 }
