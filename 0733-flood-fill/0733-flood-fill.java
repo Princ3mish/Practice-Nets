fe4c58ff81fa5bddf0b351ac2aa5b054ea0ec475
Prince Mishra <pric3mxd@gmail.com>
2026-06-11 03:58:24 +0530

    Time: 0 ms (100%), Space: 46.8 MB (60.33%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -6,12 +6,12 @@ class Solution {
         int rows = image.length;
         int cols = image[0].length;
         int initialcolor = image[sr][sc];
-        dfs(image , sr , sc , rows , cols , initialcolor , color);
+        dfs(image , sr , sc ,rows , cols , initialcolor , color);
         return image;
     }
     private void dfs(int[][] image , int r , int c , int rows , int cols , int initialcolor , int color){
-        if(r < 0 || r >= rows || c < 0 || c >= cols || image[r][c] != initialcolor){
-            return;
+        if(r < 0 ||  r >= rows || c < 0 || c >=cols || image[r][c] != initialcolor){
+            return; 
         }
         image[r][c] = color;
         dfs(image , r+1 , c , rows , cols , initialcolor , color);
