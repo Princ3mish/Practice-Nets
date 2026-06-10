552eca4424a522ed530ccf1bb969e114efb54314
Prince Mishra <pric3mxd@gmail.com>
2026-06-11 03:43:05 +0530

    Time: 0 ms (100%), Space: 46.9 MB (41.76%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,30 +1,22 @@
 class Solution {
-    private void dfs(int[][] image , int r , int c , int originalColor , int newColor){
-        if(r < 0 || r >= image.length || c < 0 || c >= image[0].length){
-            return;
-        }
-        if(image[r][c] != originalColor){
-            return;
-        }
-        image[r][c] = newColor;
-
-
-        dfs(image , r+1 , c , originalColor , newColor);
-        dfs(image , r-1 , c , originalColor , newColor);
-        dfs(image , r , c+1 , originalColor , newColor);
-        dfs(image , r , c-1 , originalColor , newColor);
-    }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int originalColor = image[sr][sc];
-
-        if(originalColor == color){
+        if(image[sr][sc] == color){
             return image;
         }
-
-        dfs(image , sr , sc , originalColor , color);
-
+        int rows = image.length;
+        int cols = image[0].length;
+        int initialcolor = image[sr][sc];
+        dfs(image , sr , sc , rows , cols , initialcolor , color);
         return image;
-
-        
+    }
+    private void dfs(int[][] image , int r , int c , int rows , int cols , int initialcolor , int color){
+        if(r < 0 || r >= rows || c < 0 || c >= cols || image[r][c] != initialcolor){
+            return;
+        }
+        image[r][c] = color;
+        dfs(image , r+1 , c , rows , cols , initialcolor , color);
+        dfs(image , r-1 , c , rows , cols , initialcolor , color);
+        dfs(image , r , c+1 , rows , cols , initialcolor , color);
+        dfs(image , r , c-1 , rows , cols , initialcolor , color);
     }
 }
