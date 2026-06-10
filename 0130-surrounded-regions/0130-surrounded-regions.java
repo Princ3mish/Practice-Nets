74702fdea27f8dfb4e7dc113dae2ceb08bf5a514
Prince Mishra <pric3mxd@gmail.com>
2026-06-11 04:36:17 +0530

    Time: 1 ms (99.73%), Space: 47.8 MB (55.11%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,58 +1,46 @@
 class Solution {
-
-    public void solve(char[][] board) {
-
+    public char[][] solve(char[][] board) {
+        if(board == null || board.length == 0)return board;
         int rows = board.length;
-        int column = board[0].length;
-
-        for(int r = 0; r < rows; r++) {
-
-            if(board[r][0] == 'O')
-                dfs(board, r, 0);
-
-            if(board[r][column - 1] == 'O')
-                dfs(board, r, column - 1);
+        int cols = board[0].length;
+        for(int r = 0 ; r < rows ; r++){
+            if(board[r][0] == 'O'){
+                dfs(board , r , 0 , rows , cols);
+            }
+            if(board[r][cols-1] == 'O'){
+                dfs(board , r , cols-1 , rows , cols);
+            }
         }
-
-        for(int c = 0; c < column; c++) {
-
-            if(board[0][c] == 'O')
-                dfs(board, 0, c);
-
-            if(board[rows - 1][c] == 'O')
-                dfs(board, rows - 1, c);
+        for(int c  = 0 ; c < cols ; c++){
+            if(board[0][c] == 'O'){
+                dfs(board , 0 , c , rows , cols);
+            }
+            if(board[0][c] == 'O'){
+                dfs(board , 0 , c , rows , cols);
+            }
+            if(board[rows - 1][c] == 'O'){
+                dfs(board , rows - 1 , c , rows , cols);
+            }
         }
-
-        for(int r = 0; r < rows; r++) {
-
-            for(int c = 0; c < column; c++) {
-
-                if(board[r][c] == 'O') {
+        for(int r = 0 ; r < rows ; r++){
+            for(int c = 0 ; c < cols ; c++){
+                if(board[r][c] == 'O'){
                     board[r][c] = 'X';
-                }
-
-                else if(board[r][c] == '#') {
+                }else if(board[r][c] == '#'){
                     board[r][c] = 'O';
                 }
             }
         }
+        return board;
     }
-
-
-    private void dfs(char[][] board, int r, int c) {
-
-        int rows = board.length;
-        int column = board[0].length;
-
-        if(r < 0 || c < 0 || r >= rows || c >= column || board[r][c] != 'O') {
-            return;
+    private void dfs(char[][] board , int r , int c , int rows , int cols){
+        if(r < 0 || r >= rows || c < 0 || c >= cols || board[r][c] != 'O'){
+            return ;
         }
-
         board[r][c] = '#';
-
-        dfs(board, r + 1, c);
-        dfs(board, r - 1, c);
-        dfs(board, r, c + 1);
-        dfs(board, r, c - 1);
+        dfs(board , r+1 , c , rows , cols );
+        dfs(board , r-1 , c , rows , cols );
+        dfs(board , r , c+1 , rows , cols );
+        dfs(board , r , c-1 , rows , cols );
     }
 }
