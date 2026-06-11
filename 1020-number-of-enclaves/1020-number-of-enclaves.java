44709fb9d0147b599d4f04039f8fdbb1bd77a9db
Prince Mishra <pric3mxd@gmail.com>
2026-06-11 07:07:56 +0530

    Time: 7 ms (84.34%), Space: 62.1 MB (18.15%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.java
+++ 1020-number-of-enclaves/1020-number-of-enclaves.java
@@ -1,45 +1,42 @@
 class Solution {
     public int numEnclaves(int[][] grid) {
-    int rows = grid.length;
-    int columns = grid[0].length;
-
-    for ( int r = 0 ; r < rows ; r++){
-    if( grid[r][0]==1)
-    dfs(grid, r , 0);
-
-    if( grid[r][columns-1]==1)
-    dfs(grid , r , columns - 1 );
-       
-    }
-    for ( int c = 0 ; c < columns ; c++){
-        if(grid[0][c] == 1)
-        dfs(grid , 0 , c);
-        if(grid[rows-1][c] == 1)
-        dfs(grid , rows-1 , c );
-    }
-    int count = 0 ; 
-    for( int r = 0 ; r < rows ; r++){
-        for( int c = 0 ; c < columns ; c++){
-            if(grid[r][c] == 1)
-            count++;
+        if(grid == null || grid.length == 0)return 0;
+        int rows = grid.length;
+        int cols = grid[0].length;
+        for(int r = 0 ; r < rows ; r++){
+            if(grid[r][0] == 1){
+                dfs(grid , r , 0 , rows , cols);
+            }
+            if(grid[r][cols - 1] == 1){
+                dfs(grid , r , cols - 1 , rows , cols );
+            }
         }
+        for(int c = 0 ; c < cols ; c++){
+            if(grid[0][c] == 1){
+                dfs(grid , 0 , c , rows , cols);
+            }
+            if(grid[rows - 1][c] == 1){
+                dfs(grid , rows - 1 , c , rows , cols);
+            }
+        }
+        int enc = 0;
+        for(int r = 0 ; r < rows ; r++){
+            for(int c = 0 ; c < cols ; c++){
+                if(grid[r][c] == 1){
+                    enc++;
+                }
+            }
+        }
+        return enc;
     }
-    return count;
-
-}
-private void dfs( int[][] grid , int r , int c){
-    int rows = grid.length ;
-    int columns = grid[0].length;
-
-    if( r < 0 || c < 0 || r >= rows || c >= columns ||grid[r][c] == 0 ){
-        return ; 
-    } 
-    grid[r][c] = 0 ; 
-
-    dfs(grid , r+1 , c);
-    dfs(grid , r-1 , c);
-    dfs(grid , r , c+1);
-    dfs(grid , r , c-1);
-
-}
+    private void  dfs(int[][] grid , int r , int c , int rows , int cols){
+        if(r < 0 || r >= rows || c < 0 || c >= cols || grid[r][c] != 1){
+            return;
+        }
+        grid[r][c] = 0;
+        dfs(grid , r+1 , c , rows, cols);
+        dfs(grid , r-1 , c , rows, cols);
+        dfs(grid , r , c+1 , rows, cols);
+        dfs(grid , r , c-1 , rows, cols);
+    }
 }
