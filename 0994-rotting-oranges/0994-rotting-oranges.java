01e92ea91b5cbcd5561205a8c14ffd5cfd096bfa
Prince Mishra <pric3mxd@gmail.com>
2026-06-14 04:23:21 +0530

    Time: 1 ms (100%), Space: 44.1 MB (71.5%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.java
+++ 0994-rotting-oranges/0994-rotting-oranges.java
@@ -1,48 +1,41 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
-        Queue<int[]> q = new ArrayDeque<>();
-
-        int fresh = 0 ; 
-        int time = 0 ; 
-
-        for ( int r = 0 ; r < grid.length ; r++){
-            for ( int c = 0 ; c < grid[0].length ; c++){
-                if( grid[r][c] == 1){
+        int totalrows = grid.length;
+        int totalcols = grid[0].length;
+        Queue<int[]> queue = new ArrayDeque<>();
+        int fresh = 0;
+        for(int r = 0 ; r < totalrows ; r++){
+            for(int c = 0 ; c < totalcols ; c++){
+                if(grid[r][c] ==2){
+                    queue.add(new int[]{r,c});
+                }else if(grid[r][c] == 1){
                     fresh++;
-                }
-
-                if(grid[r][c] == 2){
-                    q.offer(new int[]{r,c});//new starting points hai bfs ka jahan se traversal karna chaloo karrna hai 
                 }
             }
         }
-        int[][] directions = {{0,1},{0,-1},{1,0},{-1,0}};
+        if(fresh == 0)return 0;
+        int minutes = 0;
+        int[] drow = {-1,1,0,0};
+        int[] dcol = {0,0,-1,1};
+        while(!queue.isEmpty()){
+            int curr = queue.size();
+            for(int i = 0 ; i < curr ; i++){
+                int[] current = queue.poll();
+                int r = current[0];
+                int c = current[1];
 
-        while ( fresh > 0 && !q.isEmpty()){
-            int length = q.size();
-            for( int i  = 0 ; i < length ; i++){
-                int[] curr = q.poll();
-                int r = curr[0];
-                int c = curr[1];
-
-                for (int[] dir:directions){
-                    int row = r + dir[0];
-                    int col = c + dir[1];
-
-                    if (row >= 0 && row<grid.length && col>=0 && col<grid[0].length && grid[row][col] == 1){
-                        grid[row][col] = 2;
-
-                        q.offer(new int[]{row , col});
-
+                for(int d = 0 ; d < 4 ; d++){
+                    int nrow = r + drow[d];
+                    int ncol = c + dcol[d];
+                    if(nrow >= 0 && nrow < totalrows && ncol >= 0 && ncol < totalcols && grid[nrow][ncol] == 1){
+                        grid[nrow][ncol] = 2;
                         fresh--;
-
+                        queue.add(new int[]{nrow,ncol});
                     }
                 }
             }
-            time++;
-
-
+            minutes++;
         }
-        return fresh == 0 ? time: -1;
+        return fresh == 0 ? minutes - 1 : -1;
     }
 }
