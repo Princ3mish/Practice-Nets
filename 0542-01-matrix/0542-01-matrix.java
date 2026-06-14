7bba32417819fc9bbe0c1f96536670e33a4eca16
Prince Mishra <pric3mxd@gmail.com>
2026-06-14 06:03:18 +0530

    Time: 13 ms (91.97%), Space: 49.2 MB (40.53%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,55 +1,38 @@
-import java.util.*;
-
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-
-       
-        int rows = mat.length;
-
-     
-        int cols = mat[0].length;
-        Queue<int[]> queue = new LinkedList<>();
-        int[][] directions = {
-            {1, 0},   
-            {-1, 0},  
-            {0, 1}, 
-            {0, -1}   
-        };
-
-
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
-
-                if (mat[r][c] == 0) {
-                    queue.offer(new int[]{r, c});
-
-                } else {
-
-                    mat[r][c] = -1;
+        int totalrows = mat.length;
+        int totalcols = mat[0].length;
+        int[][]dist = new int[totalrows][totalcols];
+        Queue<int[]> queue = new ArrayDeque<>();
+        for(int r = 0 ; r < totalrows ; r++){
+            for(int c = 0 ; c < totalcols ; c++){
+                if(mat[r][c] == 0){
+                    dist[r][c] = 0;
+                    queue.add(new int[]{r,c});
+                }else{
+                    dist[r][c] = -1;
                 }
             }
         }
-
-        while (!queue.isEmpty()) {
+        int[] drow = {-1,1,0,0};
+        int[] dcol = {0,0,-1,1};
+        while(!queue.isEmpty()){
             int[] current = queue.poll();
+            int r = current[0];
+            int c = current[1];
+            for(int d = 0 ; d < 4 ;d++){
+                int nrow = r + drow[d];
+                int ncol = c + dcol[d];
+                if(nrow >= 0 && nrow < totalrows && ncol >= 0 && ncol < totalcols && dist[nrow][ncol] == -1){
+                    dist[nrow][ncol] = dist[r][c] + 1;
+                    queue.add(new int[]{nrow,ncol});
 
-            int r = current[0];  
-            int c = current[1];  
-            for (int[] dir : directions) {
-
-                int newRow = r + dir[0];
-                int newCol = c + dir[1];
-
-                if (newRow >= 0 && newRow < rows &&
-                    newCol >= 0 && newCol < cols &&
-                    mat[newRow][newCol] == -1) {
-
-                    mat[newRow][newCol] = mat[r][c] + 1;
-
-                    queue.offer(new int[]{newRow, newCol});
                 }
             }
         }
-        return mat;
+        return dist;
+
+
+        
     }
 }
