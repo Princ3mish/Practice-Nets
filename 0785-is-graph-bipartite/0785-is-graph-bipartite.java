213a2673c64bba6015f38e5da1a772cc4e6a118c
Prince Mishra <pric3mxd@gmail.com>
2026-06-25 06:20:26 +0530

    Time: 1 ms (61.57%), Space: 47.4 MB (19.1%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,40 +1,31 @@
-import java.util.*;
-
 class Solution {
-
     public boolean isBipartite(int[][] graph) {
-
-        int n = graph.length;
-
-        int[] color = new int[n];
-
-        for(int start = 0; start < n; start++) {
-            if(color[start] != 0)
-                continue;
-
-            Queue<Integer> queue = new LinkedList<>();
-
-
-            color[start] = 1;
-
-            queue.offer(start);
-
-
-            while(!queue.isEmpty()) {
-
-                int node = queue.poll();
-
-                for(int neighbor : graph[node]) {
-                    if(color[neighbor] == 0) {
-
-                        color[neighbor] = -color[node];
-
-                        queue.offer(neighbor);
-                    }
-                    else if(color[neighbor] == color[node]) {
-
-                        return false;
-                    }
+        int N = graph.length;
+        int[] color = new int[N];
+        Arrays.fill(color , -1);
+        for(int i = 0 ; i < N ; i++){
+            if(color[i] == -1){
+                if(!bfs(i,graph,color)){
+                    return false;
+                }
+            }
+        }
+        return true;
+        
+    }
+    private boolean bfs(int start ,int[][] graph , int[] color){
+        Queue<Integer> queue = new LinkedList<>();
+        color[start] = 0;
+        queue.add(start);
+        while(!queue.isEmpty()){
+            int node = queue.poll();
+            for(int n : graph[node]){
+                if(color[n] == -1){
+                    color[n] = 1 - color[node];
+                    queue.add(n);
+                }
+                else if (color[n] == color[node]){
+                    return false;
                 }
             }
         }
