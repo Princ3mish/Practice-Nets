46e0b90a9b5b6ba593b2b564d36a5604e4227507
Prince Mishra <pric3mxd@gmail.com>
2026-06-30 06:18:29 +0530

    Time: 27 ms (31.96%), Space: 60.4 MB (85.2%) - LeetHub

--- 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
+++ 0802-find-eventual-safe-states/0802-find-eventual-safe-states.java
@@ -1,26 +1,35 @@
 class Solution {
     public List<Integer> eventualSafeNodes(int[][] graph) {
         int n = graph.length;
-        int[] state = new int[n];
-        List<Integer> result = new ArrayList<>();
-        for( int i = 0 ; i < n ; i++){
-            if(isSafe(graph , i , state)){
-                result.add(i);
+        List<List<Integer>> adjRev = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            adjRev.add(new ArrayList<>());
+        }
+        int[] inDegree = new int[n];
+        for (int i = 0; i < n; i++) {
+            for (int neighbor : graph[i]) {
+                adjRev.get(neighbor).add(i);
+                inDegree[i]++; 
             }
         }
-        return result;  
-    }
-    private boolean isSafe(int[][] graph , int node , int[] state){
-        if(state[node]!= 0){
-            return state[node] == 2;
-        }
-        state[node] = 1;
-        for( int neighbour : graph[node]){
-            if(!isSafe(graph , neighbour , state)){
-                return false ;
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            if (inDegree[i] == 0) {
+                queue.add(i);
             }
         }
-        state[node] = 2;
-        return true;
+        List<Integer> safeNodes = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            safeNodes.add(node);
+            for (int neighbor : adjRev.get(node)) {
+                inDegree[neighbor]--;
+                if (inDegree[neighbor] == 0) {
+                    queue.add(neighbor);
+                }
+            }
+        }
+        Collections.sort(safeNodes);
+        return safeNodes;
     }
 }
