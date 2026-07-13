ef26d2661dfaca0ae96446eea3826a64749cf59f
Prince Mishra <pric3mxd@gmail.com>
2026-07-13 09:46:21 +0530

    Time: 0 ms (100%), Space: 42.1 MB (94.86%) - LeetHub

---
+++ 1291-sequential-digits/1291-sequential-digits.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<Integer> sequentialDigits(int low, int high) {
+        List<Integer> result =  new ArrayList<>();
+        String digits = "123456789";
+        int lowlen = String.valueOf(low).length();
+        int highlen = String.valueOf(high).length();
+        for(int length = lowlen ; length <=highlen ; length++){
+            for(int start = 0  ; start <= 9 - length ; start++){
+                String sub = digits.substring(start , start+length);
+                int num = Integer.parseInt(sub);
+                if(num >= low && num <=high){
+                    result.add(num);
+                }
+            }
+        }
+        return result;
+
+
+        
+    }
+}
