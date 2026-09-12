85e8cc2d5ab39e37b08ed81ba287d3b107e94afa
Prince Mishra <pric3mxd@gmail.com>
2026-09-12 18:53:22 +0530

    Time: 3 ms (85.97%), Space: 43.1 MB (72.54%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,17 +1,20 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
+        Stack<Character> st = new Stack<>();
         for(char c : s.toCharArray()){
             if(c == '('){
-                stack.push(')');
-            }else if(c == '{'){
-                stack.push('}');
-            }else if(c == '['){
-                stack.push(']');
-            }else if (stack.isEmpty() || stack.pop() != c){
+                st.push(')');
+            }
+            else if(c == '['){
+                st.push(']');
+            }
+            else if(c == '{'){
+                st.push('}');
+            }
+            else if(st.isEmpty() || st.pop() != c){
                 return false;
             }
         }
-        return stack.isEmpty();
+        return st.isEmpty();
     }
 }
