f8390455e21c10a125bcdf0f0095d4af18e2be42
Prince Mishra <pric3mxd@gmail.com>
2026-09-14 18:13:24 +0530

    Time: 14 ms (43.81%), Space: 51.2 MB (94.3%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,33 +1,17 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        List<Integer> result = new ArrayList<>();
-        if (nums == null || nums.length == 0) return result;
-        int candidate1 = 0 , count1 =  0;
-        int candidate2 = 1 , count2 = 0 ;
-        for( int num : nums){
-            if(num == candidate1 ) {count1++;}
-            else if(num == candidate2){count2++;}
-            else if(count1 == 0){
-                candidate1 = num;
-                count1 = 1;
-            }
-            else if(count2 == 0){
-                candidate2 = num;
-                count2 = 1;
-            }else{
-                count1--;
-                count2--;
-            }
-        }
-         count1 = 0;
-         count2 = 0;
-         for (int num : nums) {
-            if (num == candidate1) count1++;
-            else if (num == candidate2) count2++;
-        }
+        List<Integer> res = new ArrayList<>();
+        Map<Integer , Integer> map = new HashMap<>();
         int n = nums.length;
-        if(count1 > n/3) result.add(candidate1);
-        if(count2 > n/3) result.add(candidate2);
-        return result;
+        int thres =  n / 3;
+        for(int v : nums){
+            map.put(v,map.getOrDefault(v,0)+1);
+        }
+        for(Map.Entry<Integer,Integer> entry : map.entrySet()){
+            if(entry.getValue() > thres){
+                res.add(entry.getKey());
+            }
+        }
+        return res;
     }
 }
